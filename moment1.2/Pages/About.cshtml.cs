927b0e088ce9bb12d8744990c1ff7051881a25e3
r8now <hosein_100@hotmail.com>
2022-01-27 18:46:27 +0100

    first commit

---
+++ moment1.2/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace moment1._2.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ moment1.2/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+const menu = document.querySelector('.menu');
+
+const nav = document.querySelector('nav');
+
+menu.addEventListener('click', () => {
+
+    menu.classList.toggle('open')
+    nav.classList.toggle('navbar')
+});
